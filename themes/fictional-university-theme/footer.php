cbf904e046e5426efbf049066047d49886757c88
chia <chanchia@me.com>
2020-08-09 13:41:18 -0400

    add search bar

--- themes/fictional-university-theme/footer.php
+++ themes/fictional-university-theme/footer.php
@@ -74,6 +74,14 @@
       </div>
     </footer>
 
+    <div class="search-overlay">
+      <div class="container">
+        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+        <input type="text" class="search-term" placeholder="What art you looking for?" id="search-term">
+        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+      </div>
+    </div>
+
 <? wp_footer(); ?>
 </body>
 </html>
